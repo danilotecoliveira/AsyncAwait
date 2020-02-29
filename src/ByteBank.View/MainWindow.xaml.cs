a378f106c31fd6955a67722e0259634cf135e03a
Danilo Oliveira <danilo.oliveira@bbnk.com.br>
2020-02-28 23:51:56 -0300

    Criando threads

--- src/ByteBank.View/MainWindow.xaml.cs
+++ src/ByteBank.View/MainWindow.xaml.cs
@@ -1,21 +1,10 @@
-﻿using ByteBank.Core.Model;
-using ByteBank.Core.Repository;
+﻿using System;
+using System.Windows;
 using ByteBank.Core.Service;
-using System;
+using ByteBank.Core.Repository;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ByteBank.View
 {
@@ -36,17 +25,37 @@ namespace ByteBank.View
         {
             var contas = r_Repositorio.GetContaClientes();
 
+            var contas_parte1 = contas.Take(contas.Count() / 2);
+            var contas_parte2 = contas.Skip(contas.Count() / 2);
+
             var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            foreach (var conta in contas)
+            Thread thread_parte1 = new Thread(() => 
             {
-                var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                resultado.Add(resultadoConta);
-            }
+                foreach (var conta in contas_parte1)
+                {
+                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoConta);
+                }
+            });
+
+            Thread thread_parte2 = new Thread(() =>
+            {
+                foreach (var conta in contas_parte2)
+                {
+                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(resultadoConta);
+                }
+            });
+
+            thread_parte1.Start();
+            thread_parte2.Start();
+
+            //thread_parte1.IsAlive;
 
             var fim = DateTime.Now;
 
