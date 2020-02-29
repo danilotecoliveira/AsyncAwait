64b235076ab65074169bc2eec7538460a1e89060
Danilo Oliveira <danilo.oliveira@bbnk.com.br>
2020-02-29 00:16:17 -0300

    Criando e executando Tasks

--- src/ByteBank.View/MainWindow.xaml.cs
+++ src/ByteBank.View/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using ByteBank.Core.Repository;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ByteBank.View
 {
@@ -24,65 +25,22 @@ namespace ByteBank.View
         private void BtnProcessar_Click(object sender, RoutedEventArgs e)
         {
             var contas = r_Repositorio.GetContaClientes();
-
-            var contasPorThread = contas.Count() / 4;
-
-            var contas_parte1 = contas.Take(contasPorThread);
-            var contas_parte2 = contas.Skip(contasPorThread).Take(contasPorThread);
-            var contas_parte3 = contas.Skip(contasPorThread * 2).Take(contasPorThread);
-            var contas_parte4 = contas.Skip(contasPorThread * 3);
-
             var resultado = new List<string>();
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
 
-            Thread thread_parte1 = new Thread(() => 
+            var contasTarefas = contas.Select(conta =>
             {
-                foreach (var conta in contas_parte1)
+                return Task.Factory.StartNew(() =>
                 {
                     var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
                     resultado.Add(resultadoConta);
-                }
-            });
+                });
+            }).ToArray();
 
-            Thread thread_parte2 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte2)
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
-
-            Thread thread_parte3 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte3)
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
-
-            Thread thread_parte4 = new Thread(() =>
-            {
-                foreach (var conta in contas_parte4)
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                }
-            });
-
-            thread_parte1.Start();
-            thread_parte2.Start();
-            thread_parte3.Start();
-            thread_parte4.Start();
-
-            while (thread_parte1.IsAlive || thread_parte2.IsAlive || thread_parte3.IsAlive || thread_parte4.IsAlive)
-            {
-                Thread.Sleep(250);
-            }
+            Task.WaitAll(contasTarefas);
 
             var fim = DateTime.Now;
 
